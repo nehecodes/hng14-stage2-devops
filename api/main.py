53e6a45c91ca010ac196d2072129a277d4153030
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-04-20 20:09:03 +0100

    fix: import configuration data from environment
    fix: implement try/except error handling
    
    fix: rename redis connection variable

--- api/main.py
+++ api/main.py
@@ -1,22 +1,36 @@
 from fastapi import FastAPI
+from config import settings
 import redis
 import uuid
-import os
+
 
 app = FastAPI()
+host = settings.redis_host
+port = settings.redis_port
+redis_conn = redis.Redis(
+    host=host,
+    port=port,
+    decode_responses=True,
+)
 
-r = redis.Redis(host="localhost", port=6379)
 
 @app.post("/jobs")
-def create_job():
+async def create_job():
     job_id = str(uuid.uuid4())
-    r.lpush("job", job_id)
-    r.hset(f"job:{job_id}", "status", "queued")
-    return {"job_id": job_id}
+    try:
+        redis_conn.lpush("job", job_id)
+        redis_conn.hset(f"job:{job_id}", "status", "queued")
+        return {"job_id": job_id}
+    except Exception as e:
+        return {"message": f"failed to queue job {job_id}: {e}"}
+
 
 @app.get("/jobs/{job_id}")
-def get_job(job_id: str):
-    status = r.hget(f"job:{job_id}", "status")
-    if not status:
-        return {"error": "not found"}
-    return {"job_id": job_id, "status": status.decode()}
+async def get_job(job_id: str):
+    try:
+        status = redis_conn.hget(f"job:{job_id}", "status")
+        if not status:
+            return {"error": "not found"}
+        return {"job_id": job_id, "status": status}
+    except Exception as e:
+        return {"message": f"some error occured: {e}"}
