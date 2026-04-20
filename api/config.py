59292e2d5f8d7532f13f52afcb9f83f2f9798062
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-04-20 20:01:54 +0100

    config: add configuration file

---
+++ api/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+from pathlib import Path
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent / ".env",
+        case_sensitive=False,
+    )
+    redis_host: str
+    redis_port: int
+    redis_password: SecretStr
+
+
+settings = Settings()
