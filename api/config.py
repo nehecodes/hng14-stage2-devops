91a67ea5b62f15128eaddc5ef5dc5dcab980fd14
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-04-22 08:08:19 +0100

    config: add config for secrets

--- api/config.py
+++ api/config.py
@@ -6,6 +6,8 @@ from pathlib import Path
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env",
+        secrets_dir="/run/secrets",
+        env_file_encoding="utf-8",
         case_sensitive=False,
     )
     redis_host: str
